76fa78512be7672e61244e1eaa2c8e427904369d
Aromal S <aromalponmana@gmail.com>
2021-02-20 23:05:29 +0530

    Update InsertionSort.c

--- Sorting Algorithms/InsertionSort.c
+++ Sorting Algorithms/InsertionSort.c
@@ -1,34 +1,38 @@
-#include <stdio.h>
+#include<stdio.h>
 
-#define size 5
-void insertion_sort(int arr[], int n);
-void main()
+void InsertionSort(int arr[],int n);
+
+int main()
 {
-	int arr[size], i, n;
-	printf("\n Enter the number of elements in the array: ");
-	scanf("%d", &n);
-	printf("\n Enter the elements of the array: ");
-	for(i=0;i<n;i++)
-	{
-		scanf("%d", &arr[i]);
-	}
-	insertion_sort(arr, n);
-	printf("\n The sorted array is: \n");
-	for(i=0;i<n;i++)
-		printf(" %d\t", arr[i]);
+    int arr[100],n,i;
+    printf("Enter the number of elements to be sorted: ");
+    scanf("%d",&n);
+    printf("Enter the  elements : ");
+    for(i=0;i<n;++i)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    InsertionSort(arr,n);
+    printf("Sorted Array :\n");
+    for(i=0;i<n;++i)
+    {
+        printf("%d  ",arr[i]);
+        
+    }
 }
-void insertion_sort(int arr[], int n)
-{
-	int i, j, temp;
-	for(i=1;i<n;i++)
-	{
-		temp = arr[i];
-		j = i-1;
-		while((temp < arr[j]) && (j>=0))
-		{
-			arr[j+1] = arr[j];
-			j--;
-		}
-		arr[j+1] = temp;
-	}
+
+void InsertionSort(int arr[],int n)
+{	
+    for (int i = 1; i <n; ++i)
+    {
+        int val=arr[i],j=i-1;
+        while(j>=0 && arr[j]>val)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=val;
+    }
 }
+
