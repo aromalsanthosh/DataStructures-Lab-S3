f07d2538ab79614d53457061e08cafb016fa0087
Aromal S <aromalponmana@gmail.com>
2021-02-20 23:13:17 +0530

    Update InsertionSort.c

--- Sorting Algorithms/InsertionSort.c
+++ Sorting Algorithms/InsertionSort.c
@@ -24,7 +24,7 @@ int main()
 
 void InsertionSort(int arr[],int n)
 {	
-    for (int i = 1; i <n; ++i)
+    for (int i = 0; i <n-1; ++i)
     {
         int val=arr[i],j=i-1;
         while(j>=0 && arr[j]>val)
