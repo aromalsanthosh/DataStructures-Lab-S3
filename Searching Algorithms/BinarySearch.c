ff4ce631bf55f794e01c7b28f9a57d75d6259fc6
Aromal S <aromalponmana@gmail.com>
2021-02-23 20:52:48 +0530

    Update BinarySearch.c

--- Searching Algorithms/BinarySearch.c
+++ Searching Algorithms/BinarySearch.c
@@ -1,63 +1,73 @@
-#include <stdio.h>
-#include <stdlib.h>
+// Binary Search in C
 
-#define size 10 
-int smallest(int arr[], int k, int n); 
-void selection_sort(int arr[], int n); 
-int main() 
-{
-	int arr[size], num, i, n, beg, end, mid, found=0;
-	printf("\n Enter the number of elements in the array: ");
+#include <stdio.h>
+void bubbleSort(int arr[], int n);
+
+int binarySearch(int array[], int x, int low, int high) {
+  // Repeat until the pointers low and high meet each other
+  while (low <= high) {
+    int mid = low + (high - low) / 2;
+
+    if (array[mid] == x)
+      return mid;
+
+    if (array[mid] < x)
+      low = mid + 1;
+
+    else
+      high = mid - 1;
+  }
+
+  return -1;
+}
+
+int main(void) {
+  int arr[100],n,key;
+
+	printf("\nEnter the number of elements in the array : ");
 	scanf("%d", &n);
-	printf("\n Enter the elements: ");
-	for(i=0;i<n;i++)
+	printf("\nEnter the elements: ");
+	for(int i=0;i<n;i++)
+	{
 		scanf("%d", &arr[i]);
-	selection_sort(arr, n); 
-	printf("\n The sorted array is: \n");
-	for(i=0;i<n;i++)
-		printf(" %d\t", arr[i]);
-	printf("\n\n Enter the number that has to be searched: ");
-	scanf("%d", &num);
-	beg = 0, end = n-1;
-	while(beg<=end)
-	{
-		mid = (beg + end)/2;
-		if (arr[mid] == num)
-		{
-			printf("\n %d is present in the array at position %d", num, mid+1);
-			found =1;
-			break;
-		}
-		else if (arr[mid]>num)
-			end = mid-1;
-		else
-			beg = mid+1;
 	}
-	if (beg > end && found == 0)
-	printf("\n %d does not exist in the array", num);
-	return 0;
+	printf("\nEnter the number that has to be searched : ");
+	scanf("%d", &key);
+
+  bubbleSort(arr,n);
+
+  
+  int result = binarySearch(arr, key, 0, n - 1);
+  if (result == -1)
+    printf("Not found");
+  else
+    printf("\nElement is found at position %d", result+1);
+  return 0;
 }
-int smallest(int arr[], int k, int n)
+
+void bubbleSort(int arr[], int n)
 {
-	int pos = k, small=arr[k], i;
-	for(i=k+1;i<n;i++)
-	{
-		if(arr[i]< small)
-		{
-			small = arr[i];
-			pos = i;
-		}
-	}
-	return pos;
+    int i, j, temp;
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n-i-1; j++)
+        {
+            if( arr[j] > arr[j+1])
+            {
+                // swap the elements
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            } 
+        }
+    }
+    
+    // print the sorted array
+    printf("Sorted Array: ");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d  ", arr[i]);
+    }
 }
-void selection_sort(int arr[],int n)
-{
-	int k, pos, temp;
-	for(k=0;k<n;k++)
-	{
-		pos = smallest(arr, k, n);
-		temp = arr[k];
-		arr[k] = arr[pos];
-		arr[pos] = temp;
-	}
-}
+
+
